ad0970635e25c584b73c303ef111ab502937982d
Ajaybabu21 <ajaybabukanjirakkattu2@gmail.com>
2023-11-04 17:14:10 +0530

    first commit

---
+++ src/quizData.js
@@ -0,0 +1,11 @@
+const quizData = [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "Berlin", "London", "Madrid"],
+      correctAnswer: "Paris",
+    },
+    // Add more questions here
+  ];
+  
+  export default quizData;
+  
